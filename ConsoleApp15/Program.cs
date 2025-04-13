568a5d62e783671d43755ced91a3c772abdb47bb
alexkopac <alexkopach08@gmail.com>
2025-04-13 12:07:51 +0300

    Add files via upload

---
+++ ConsoleApp15/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    public interface ITextFormatter
+    {
+        string Format(string text);
+    }
+
+   
+    public class UpperCaseFormatter : ITextFormatter
+    {
+        public string Format(string text)
+        {
+            return text.ToUpper();
+        }
+    }
+
+    
+    public class LowerCaseFormatter : ITextFormatter
+    {
+        public string Format(string text)
+        {
+            return text.ToLower();
+        }
+    }
+
+    
+    public class TitleCaseFormatter : ITextFormatter
+    {
+        public string Format(string text)
+        {
+            TextInfo textInfo = CultureInfo.CurrentCulture.TextInfo;
+            return textInfo.ToTitleCase(text);
+        }
+    }
+
+    
+    public class TextEditor
+    {
+        private ITextFormatter formatter;
+
+        public TextEditor(ITextFormatter formatter)
+        {
+            this.formatter = formatter;
+        }
+
+       
+        public void SetFormatter(ITextFormatter formatter)
+        {
+            this.formatter = formatter;
+        }
+
+       
+        public string FormatText(string text)
+        {
+            return formatter.Format(text);
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+            ITextFormatter upperCase = new UpperCaseFormatter();
+            ITextFormatter lowerCase = new LowerCaseFormatter();
+            ITextFormatter titleCase = new TitleCaseFormatter();
+
+            TextEditor editor = new TextEditor(upperCase);
+            Console.WriteLine($"Форматування у верхній регістр: {editor.FormatText("hello world")}"); 
+
+            
+            editor.SetFormatter(titleCase);
+            Console.WriteLine($"Форматування у заголовний регістр: {editor.FormatText("hello world")}"); 
+
+            
+            editor.SetFormatter(lowerCase);
+            Console.WriteLine($"Форматування у нижній регістр: {editor.FormatText("HELLO WORLD")}"); 
+        }
+    }
+}
